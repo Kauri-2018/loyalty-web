2013b0391d3f5763df2462c11fd4b64382ce68c6
Frank <xuzy213@gmail.com>
2018-05-10 10:59:32 +1200

    create homeoptions component

---
+++ client/components/HomeOptions.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+class HomeOptions extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = { }
+  }
+  render () {
+    return (
+      <div className='homeoptions'>
+        <h1>HomeOptions</h1>
+      </div>
+    )
+  }
+}
+
+export default HomeOptions
