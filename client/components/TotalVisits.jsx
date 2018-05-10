67395e08bbe9e287218f6860c29c1e0148d73301
Mayur <mayur.h.rana@gmail.com>
2018-05-10 19:07:20 +1200

    add total visits jsx component

---
+++ client/components/TotalVisits.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+class TotalVisits extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {}
+  }
+  render () {
+    return (
+      <div className='totalvisits'>
+        <h3>Total Visits: </h3>
+      </div>
+    )
+  }
+}
+
+export default TotalVisits
