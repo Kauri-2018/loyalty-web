50dc391e181920c7a1f1a121b6d843cdde04318c
Zoe Lisk <zoe.lisk@gmail.com>
2018-05-12 20:01:57 +1200

    delete commented code in utils

--- server/utils.js
+++ server/utils.js
@@ -4,24 +4,12 @@ function getDateInfo (date) {
   return moment(date).format('MMM Do YY')
 }
 
-module.exports = {
-  getDateInfo,
-  getCurrentDate
-}
-
-
-
 function getCurrentDate () {
   const currentDate = Date.now()
   return getDateInfo(currentDate)
 }
 
-// function convertDate (date) {
-//   getDateInfo(date)
-// }
-
-// module.exports = {
-//   getDateInfo,
-//   getCurrentDate,
-//   convertDate
-// }
+module.exports = {
+  getDateInfo,
+  getCurrentDate
+}
