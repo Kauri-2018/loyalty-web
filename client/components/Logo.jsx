1eee6949e1145b32ab37c1bf48ac2630724c9de3
Zoe Lisk <zoe.lisk@gmail.com>
2018-05-13 11:44:06 +1200

    add logo component to client side

---
+++ client/components/Logo.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Logo = () => {
+  return (
+    <div className='logo-container'>
+      <div className='logo'>
+        <p>This will be a logo</p>
+      </div>
+    </div>
+  )
+}
+
+export default Logo
