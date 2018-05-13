cede6a43bce00b6cd199571f7cbaed6b8edb8451
Mayur <mayur.h.rana@gmail.com>
2018-05-13 12:25:49 +1200

    add utils test for getDateInfo fn

---
+++ tests/server/utils.test.js
@@ -0,0 +1,12 @@
+const utils = require('../../server/utils')
+
+test('test setup working', () => {
+  expect(true).toBeTruthy()
+})
+
+test('returns date in correct format', () => {
+  const date = 1526170090970
+  const expected = 'May 13th 18'
+  const actual = utils.getDateInfo(date)
+  expect(actual).toBe(expected)
+})
