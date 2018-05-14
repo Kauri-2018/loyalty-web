7d4a255315fec3df53fd6023176ddaee5907a922
Zoe Lisk <zoe.lisk@gmail.com>
2018-05-15 10:50:18 +1200

    addVisit passes test

---
+++ tests/server/db/visits/countVisits.test.js
@@ -0,0 +1,13 @@
+const env = require('../test-environment')
+const db = require('../../../../server/db/admin')
+
+let testDb = null
+
+beforeEach(() => {
+  testDb = env.getTestDb()
+  return env.initialize(testDb)
+})
+
+afterEach(() => {
+  return env.cleanup(testDb)
+})
