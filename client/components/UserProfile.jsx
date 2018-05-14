23cca45832d2f1b55611338a3a512a54725abb29
Frank <xuzy213@gmail.com>
2018-05-14 17:47:06 +1200

    add userProfile component

--- client/components/UserProfile.jsx
+++ client/components/UserProfile.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
 
-const UserProfile = ({name, photo_url, email, expiry_date, membership_number}) => (
-  <div>
-    <h1>Hello</h1>
-  </div>
-)
+const UserProfile = (props) => {
+  const {name, photo_url, email, expiry_date, membership_number} = props.user
+  return (
+    <div className="card">
+      <div className="card-image">
+        <figure className="image is-4by3">
+          <img src={photo_url} alt={name} />
+        </figure>
+      </div>
+
+      <div className="card-content">
+        <div className="media">
+          <div className="media-content">
+            <p className="title is-4">{name}</p>
+            <p className="subtitle is-6">Membership num: {membership_number}</p>
+          </div>
+        </div>
+
+        <div className="content">
+          Email: {email}
+          <br/>
+          Expiry date: <time>{expiry_date}</time>
+        </div>
+      </div>
+    </div>
+  )
+}
 
 export default UserProfile
