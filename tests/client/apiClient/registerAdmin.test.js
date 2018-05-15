adfb8df1c0afe019adb87eca07cb4f22eca98c72
Zoe Lisk <zoe.lisk@gmail.com>
2018-05-16 11:18:35 +1200

    registerAdmin passes

--- tests/client/apiClient/registerAdmin.test.js
+++ tests/client/apiClient/registerAdmin.test.js
@@ -1,5 +1,3 @@
-// need to nock proper response - not a login call
-
 import nock from 'nock'
 
 import {registerAdmin} from '../../../client/apiClient'
@@ -7,25 +5,19 @@ import {registerAdmin} from '../../../client/apiClient'
 const message = 'Successfully registered'
 const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwidXNlcm5hbWUiOiJkb24iLCJpYXQiOjE1MjU2NjQ4NTgsImV4cCI6MTUyNTc1MTI1OH0.ofOMvjwHAEkbAnMK7NC2xG3RneUlGGeTE52OC-Di06w'
 
-const responseDetails = {
+const userDetails = {
   message,
   token
 }
 
-// const userDetails = {
-//   firstname: 'Mary',
-//   username: 'Mary Buchanan',
-//   password: 'monkeys'
-// }
-
 nock('http://localhost')
-  .post('api/v1/admin/register')
-  .reply(200, responseDetails)
+  .post('/api/v1/admin/register')
+  .reply(200, userDetails)
 
 test('registerAdmin sends post req to server', () => {
   const expected = token
-  return registerAdmin(responseDetails)
+  return registerAdmin(userDetails)
     .then(res => {
-      expect(res).toContain(expected)
+      expect(res).toBe(expected)
     })
 })
