1f5ae17e8a310f7f14fdb52d3736d13982093b63
Zoe Lisk <zoe.lisk@gmail.com>
2018-05-16 11:15:12 +1200

    loginAdmin passes

--- tests/client/apiClient/loginAdmin.test.js
+++ tests/client/apiClient/loginAdmin.test.js
@@ -5,19 +5,19 @@ import {loginAdmin} from '../../../client/apiClient'
 const message = 'Authentication successful'
 const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwidXNlcm5hbWUiOiJkb24iLCJpYXQiOjE1MjU2NjQ4NTgsImV4cCI6MTUyNTc1MTI1OH0.ofOMvjwHAEkbAnMK7NC2xG3RneUlGGeTE52OC-Di06w'
 
-const responseDetails = {
+const userDetails = {
   message,
   token
 }
 
 nock('http://localhost')
-  .post('api/v1/admin/login')
-  .reply(200, responseDetails)
+  .post('/api/v1/admin/login')
+  .reply(200, userDetails)
 
-test('registerAdmin sends post req to server', () => {
+test('loginAdmin sends post req to server', () => {
   const expected = token
-  return loginAdmin(responseDetails)
+  return loginAdmin(userDetails)
     .then(res => {
-      expect(res).toContain(expected)
+      expect(res.body.token).toBe(expected)
     })
 })
